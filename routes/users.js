7f6f8439a2364350362601e3361f6e810f09898b
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-12 14:18:29 -0700

    Implement new user form

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,53 @@
 const express = require("express")
 const router = express.Router()
 
+
+
+router.use(logger)
+
 router.get('/', (req, res) => {
     res.send('user list')
 })
 
 router.get('/new', (req, res) => {
-    res.send('new user form')
+    res.render("users/new")
 })
+
+router.post('/', (req, res) => {
+    const isValid = true
+    if (isValid) {
+        users.push({name: req.body.firstName, city: "None"})
+        res.redirect(`/users/${users.length - 1}`)
+    }
+    console.log(req.body.firstName)
+    res.send("Hi")
+})
+
+
+const users = [{name: "Joseph", city: "New York"}, {name: "Yuval", city: "Rome"}]
+
+router
+    .route('/:id')
+    .get((req, res) => {
+        res.send(`User: ${userInfo.name}  City: ${userInfo.city}`)
+    })
+    .put((req, res) => {
+        res.send(`Update user: ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete User: ${req.params.id}`)
+    })
+
+router.param("id", (req, res, next, id) => {
+    userInfo = users[id]
+    next()
+})
+
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express')
 const app = express()
 
 
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded( {extended: true} ))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    console.log("here you are")
     res.render('index', { text: "world"})
 })
+
+// app.use(express.static("public"))
 
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
+
 
 
 app.listen(3200)
